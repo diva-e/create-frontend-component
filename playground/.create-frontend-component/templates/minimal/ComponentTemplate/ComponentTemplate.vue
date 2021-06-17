7124df9ffa8233ab2067668065ba858a8daba6ea
Alexander Noack <alexander.noack@diva-e.com>
2021-06-17 17:02:16 +0200

    chore: add minimal flavour to playground for testing purposes

---
+++ playground/.create-frontend-component/templates/minimal/ComponentTemplate/ComponentTemplate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="<%= name %> minimal">
+    Minimal Text
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+@import '~~/assets/scss/components';
+
+</style>
