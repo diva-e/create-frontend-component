3fbbed398dde2fa5eb6a6198de149ef0a1761920
Alex <alexander.schuebl@diva-e.com>
2022-03-28 10:49:41 +0200

    feat: react class and function to same presents main folder

--- playground/.create-frontend-component/templates/react-function/ComponentTemplate/ComponentTemplate.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import <%= upperCamelCaseName %> from './<%= upperCamelCaseName %>';
-
-test('my test case', () => {
-  render(<<%= upperCamelCaseName %> />);
-});
